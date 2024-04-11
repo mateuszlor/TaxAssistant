c57c473ca94e15f6e69aa1fee7f7bff45636bb4a
Mateusz Lorencki <mateuszlorencki@gmail.com>
2024-04-12 00:14:30 +0200

    Delete & update related columns added

--- TaxAssistant.JPK.Database/Migrations/20220418205809_VersionModificationDateFields.Designer.cs
+++ TaxAssistant.JPK.Database/Migrations/20220418205809_VersionModificationDateFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaxAssistant.JPK.Database;
 
@@ -11,9 +12,10 @@ using TaxAssistant.JPK.Database;
 namespace TaxAssistant.JPK.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20220418205809_VersionModificationDateFields")]
+    partial class VersionModificationDateFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TaxAssistant.JPK.Database/Migrations/20220418205809_VersionModificationDateFields.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaxAssistant.JPK.Database.Migrations
+{
+    public partial class VersionModificationDateFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModificationDate",
+                table: "KpirSummary",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "KpirSummary",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModificationDate",
+                table: "KpirRow",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "KpirRow",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModificationDate",
+                table: "KpirPhisicalInventory",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "KpirPhisicalInventory",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModificationDate",
+                table: "KpirHeader",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "KpirHeader",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModificationDate",
+                table: "KpirControlData",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "KpirControlData",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModificationDate",
+                table: "KpirCompany",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "KpirCompany",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModificationDate",
+                table: "KpirComapnyAddress",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "KpirComapnyAddress",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModificationDate",
+                table: "Kpir",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "Kpir",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModificationDate",
+                table: "Import",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "Import",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ModificationDate",
+                table: "KpirSummary");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "KpirSummary");
+
+            migrationBuilder.DropColumn(
+                name: "ModificationDate",
+                table: "KpirRow");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "KpirRow");
+
+            migrationBuilder.DropColumn(
+                name: "ModificationDate",
+                table: "KpirPhisicalInventory");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "KpirPhisicalInventory");
+
+            migrationBuilder.DropColumn(
+                name: "ModificationDate",
+                table: "KpirHeader");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "KpirHeader");
+
+            migrationBuilder.DropColumn(
+                name: "ModificationDate",
+                table: "KpirControlData");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "KpirControlData");
+
+            migrationBuilder.DropColumn(
+                name: "ModificationDate",
+                table: "KpirCompany");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "KpirCompany");
+
+            migrationBuilder.DropColumn(
+                name: "ModificationDate",
+                table: "KpirComapnyAddress");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "KpirComapnyAddress");
+
+            migrationBuilder.DropColumn(
+                name: "ModificationDate",
+                table: "Kpir");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "Kpir");
+
+            migrationBuilder.DropColumn(
+                name: "ModificationDate",
+                table: "Import");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "Import");
+        }
+    }
+}
