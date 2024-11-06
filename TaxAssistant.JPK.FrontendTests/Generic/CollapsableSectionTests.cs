cbdd03925d34c148a53a8e5aa9c1d0ddffb9aad1
Mateusz Lorencki <mateuszlorencki@gmail.com>
2024-11-06 17:41:12 +0100

    CollapsableSection tests

---
+++ TaxAssistant.JPK.FrontendTests/Generic/CollapsableSectionTests.cs
@@ -0,0 +1,44 @@
+using FluentAssertions;
+using FluentAssertions.BUnit;
+using TaxAssistant.JPK.Client.Shared.Generic;
+
+namespace TaxAssistant.JPK.FrontendTests.Generic;
+
+public class CollapsableSectionTests : BunitTestContext
+{
+	[Test]
+	public void CollapsableSection_ShouldDisplay()
+	{
+		// Act
+		var cut = RenderComponent<CollapsableSection>(p => p
+			.Add(x => x.Title, "click")
+			.Add(x => x.Collapsed, true)
+			.AddChildContent("inner text"));
+
+		// Assert
+		cut.Find("div").Should().HaveClass("accordion");
+		cut.Find("div").FirstElementChild.Should().HaveClass("card");
+
+		cut.FindByDataTestId("card-header").Should().HaveClass("card-header");
+		cut.FindByDataTestId("card-header").FirstElementChild.Should().HaveClass("oi-plus");
+		cut.FindByDataTestId("card-header").TextContent.Should().Be("click");
+	}
+
+	[Test]
+	public void CollapsableSection_OnClick_ShouldUnfold()
+	{
+		// Act
+		var cut = RenderComponent<CollapsableSection>(p => p
+			.Add(x => x.Title, "click")
+			.Add(x => x.Collapsed, true)
+			.AddChildContent("inner text"));
+
+		cut.FindByDataTestId("card-header").Click();
+
+		// Assert
+		cut.FindByDataTestId("card-header").FirstElementChild.Should().HaveClass("oi-minus");
+
+		cut.FindByDataTestId("card-body").Should().HaveClass("card-body");
+		cut.FindByDataTestId("card-body").TextContent.Should().Be("inner text");
+	}
+}
