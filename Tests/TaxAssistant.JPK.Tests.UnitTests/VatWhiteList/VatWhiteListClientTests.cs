aa237194402ba8b88139dc5ac755549088a0091c
Mateusz Lorencki <mateusz.lorencki@orange.com>
2024-12-06 01:14:21 +0100

    More tests for VAT White List client

--- Tests/TaxAssistant.JPK.Tests.UnitTests/VatWhiteList/VatWhiteListClientTests.cs
+++ Tests/TaxAssistant.JPK.Tests.UnitTests/VatWhiteList/VatWhiteListClientTests.cs
@@ -135,5 +135,121 @@ namespace TaxAssistant.JPK.Tests.UnitTests.VatWhiteList
                 .Should()
                 .ThrowAsync<ApiException>();
         }
+
+        [Test]
+        public async Task SearchByRegon_ForValidResponse_ShouldReturn200()
+        {
+            // Arrange
+            var responseObject = new EntityResponse
+            {
+                Result = new EntityItem
+                {
+                    RequestId = "ID-1",
+                    RequestDateTime = DateTime.Today.ToShortDateString(),
+                    Subject = new Entity
+                    {
+                        Regon = "123456789"
+                    }
+                }
+            };
+            var httpClient = new HttpClient(new MockedHttpMessageHandler(true, responseObject));
+            var sut = new VatWhiteListClient(httpClient, _options);
+
+            // Act
+            var result = await sut.SearchByRegon("123456789", DateTime.Today);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Result.Should().NotBeNull();
+            result.Result.RequestId.Should().Be("ID-1");
+            result.Result.RequestDateTime.Should().Be(DateTime.Today.ToShortDateString());
+            result.Result.Subject.Should().NotBeNull();
+            result.Result.Subject!.Regon.Should().Be("123456789");
+        }
+
+        [Test]
+        public async Task SearchByRegon_ForInvalidResponse_ShouldReturn400()
+        {
+            // Arrange
+            var responseObject = new EntityResponse
+            {
+                Result = new EntityItem
+                {
+                    Subject = null
+                }
+            };
+            var httpClient = new HttpClient(new MockedHttpMessageHandler(false, responseObject));
+            var sut = new VatWhiteListClient(httpClient, _options);
+
+            // Act && Assert
+            await sut.Awaiting(x => x.SearchByRegon("123456789", DateTime.Today))
+                .Should()
+                .ThrowAsync<ApiException>();
+        }
+
+        [Test]
+        public async Task SearchByRegons_ForValidResponse_ShouldReturn200()
+        {
+            // Arrange
+            var responseObject = new EntryListResponse
+            {
+                Result = new EntryList
+                {
+                    RequestId = "ID-1",
+                    RequestDateTime = DateTime.Today.ToShortDateString(),
+                    Entries = [
+                        new Entry
+                        {
+                            Identifier = "123456789",
+                            Subjects = [
+                                new Entity { Regon = "123456789" }
+                            ]
+                        },
+                        new EntryError
+                        {
+                            Identifier = "123456789",
+                            Error = new Error {
+                                Code = "ERR-99",
+                                Message = "Some error"
+                            }
+                        }
+                    ]
+                }
+            };
+            var httpClient = new HttpClient(new MockedHttpMessageHandler(true, responseObject));
+            var sut = new VatWhiteListClient(httpClient, _options);
+
+            // Act
+            var result = await sut.SearchByRegons(["123456789"], DateTime.Today);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Result.Should().NotBeNull();
+            result.Result.RequestId.Should().Be("ID-1");
+            result.Result.RequestDateTime.Should().Be(DateTime.Today.ToShortDateString());
+            result.Result.Entries.Should().HaveCount(2);
+            result.Result.FoundEntries.Should().HaveCount(1);
+            result.Result.ErrorEntries.Should().HaveCount(1);
+        }
+
+        [Test]
+        public async Task SearchByRegons_ForInvalidResponse_ShouldReturn400()
+        {
+            // Arrange
+            var responseObject = new EntityResponse
+            {
+                Result = new EntityItem
+                {
+                    Subject = null
+                }
+            };
+            var httpClient = new HttpClient(new MockedHttpMessageHandler(false, responseObject));
+            var sut = new VatWhiteListClient(httpClient, _options);
+
+            // Act && Assert
+            await sut.Awaiting(x => x.SearchByNips(["123456789"], DateTime.Today))
+                .Should()
+                .ThrowAsync<ApiException>();
+        }
     }
 }
